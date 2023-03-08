8f13904329b7525edf48ccbe910e5abc1a4abd43
alireza <alireza.lm69@gmail.com>
2023-03-08 19:54:14 +0100

    env for primal

--- disgt.py
+++ disgt.py
@@ -1,16 +1,35 @@
 from dataclasses import dataclass
-from typing import List
-
+from typing import List, Optional
 import numpy as np
-
 from gradient_tracking import ObjectiveFunction, Agent, ComputationNetwork, GradientTracking
 from graph import IGraph
 from problems import ISparseProblem
-
 import gurobipy as gp
 from gurobipy import GRB
 
 from utils import get_sparsity
+
+
+class Environment:
+
+    def __init__(self):
+        self.primal_solver: "PrimalSolver" = Optional[None]
+
+        self.sparsity_enforcer: "SparsityEnforcer" = Optional[None]
+
+        self.iteration: "Iteration" = Optional[None]
+
+        self.problems: "List[ISparseProblem]" = Optional[None]
+
+        self.report: "Report" = Optional[None]
+
+        self.settings: "DiSGTSettings" = Optional[None]
+
+        self.timer: "Timer" = Optional[None]
+
+        self.network: "IGraph" = Optional[None]
+
+        self.task_manager: "TaskManager" = Optional[None]
 
 
 class AugLagObjective(ObjectiveFunction):
@@ -32,20 +51,19 @@ class AugLagObjective(ObjectiveFunction):
 
 
 class PrimalSolver:
-    def __init__(self, problems: List[ISparseProblem], graph: IGraph):
-        self.problems = problems
-        self.graph = graph
+    def __init__(self, env: Environment):
+        self.env = env
 
     def update_x(self, dual_vars: np.ndarray, y: np.ndarray, rho: float) -> np.ndarray:
         n = y.shape[0]
         agents = []
-        N = len(self.problems)
-        for problem in self.problems:
+        N = len(self.env.problems)
+        for problem in self.env.problems:
             obj = AugLagObjective(problem, dual_vars, y, rho, N)
 
             agents.append(Agent(obj))
 
-        network = ComputationNetwork(self.graph, agents)
+        network = ComputationNetwork(self.env.network, agents)
 
         gt = GradientTracking(network)
         gt.cfg.verbose = False
@@ -89,41 +107,59 @@ class DiSGTSettings:
     M: float = 5
 
 
+class Iteration:
+    pass
+
+
+class Report:
+    pass
+
+
+class Timer:
+    pass
+
+
+class TaskManager:
+    pass
+
+
 class DiSGT:
-    def __init__(self, p: List[ISparseProblem], net_model: IGraph, s: DiSGTSettings = DiSGTSettings()):
-        self.p = p
-        self.s = s
-        self.graph = net_model
-        self.primal_solver = PrimalSolver(p, net_model)
-        self.sparsity_enforcer = SparsityEnforcer()
-        self.kappa = p[0].get_kappa()
+    def __init__(self):
+        self.env: Environment = Environment()
 
-    def display_info(self, x, err):
-        log = f"{get_sparsity(x)} of {x.shape[0] - self.kappa}| {err: 4.4f}"
+    def set_local_problems(self, problems: List[ISparseProblem]):
+        assert len(problems) > 1
+        self.env.problems = problems
 
-        print(log)
+    def set_network_topology(self, network: IGraph):
+        self.env.network = network
 
-    def optimize(self):
-        err = 1e10
-        n = self.p[0].get_dim()
+    # def display_info(self, x, err):
+    #     log = f"{get_sparsity(x)} of {x.shape[0] - self.kappa}| {err: 4.4f}"
+    #
+    #     print(log)
 
-        lmbd = np.zeros([n, 1])
-        y = np.zeros([n, 1])
-        rho = self.s.rho
-
-        while err > self.s.eps:
-            x = self.primal_solver.update_x(lmbd, y, rho)
-            y = self.sparsity_enforcer.enforce(x, lmbd, rho, self.s.M, self.kappa)
-
-            lmbd += rho * (x - y)
-            err = np.linalg.norm(x - y)
-            # print(np.linalg.norm(x - y))
-            self.display_info(x, err)
-            f = self.compute_total_objective(x)
-
-        return x, f
-
-    def compute_total_objective(self, x: np.ndarray):
-        return sum([
-            problem.compute_obj_at(x) for problem in self.p
-        ])
+    # def optimize(self):
+    #     err = 1e10
+    #     n = self.p[0].get_dim()
+    #
+    #     lmbd = np.zeros([n, 1])
+    #     y = np.zeros([n, 1])
+    #     rho = self.s.rho
+    #
+    #     while err > self.s.eps:
+    #         x = self.primal_solver.update_x(lmbd, y, rho)
+    #         y = self.sparsity_enforcer.enforce(x, lmbd, rho, self.s.M, self.kappa)
+    #
+    #         lmbd += rho * (x - y)
+    #         err = np.linalg.norm(x - y)
+    #         # print(np.linalg.norm(x - y))
+    #         self.display_info(x, err)
+    #         f = self.compute_total_objective(x)
+    #
+    #     return x, f
+    #
+    # def compute_total_objective(self, x: np.ndarray):
+    #     return sum([
+    #         problem.compute_obj_at(x) for problem in self.p
+    #     ])
